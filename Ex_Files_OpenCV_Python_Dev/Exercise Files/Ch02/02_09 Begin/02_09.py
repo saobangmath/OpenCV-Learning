8dd9c1dbf128988fd3c333005417b3d1a5c95773
saobangmath <anhtai2672000@gmail.com>
2020-06-05 13:37:49 +0800

    Event handler

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_09 Begin/02_09.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_09 Begin/02_09.py
@@ -3,10 +3,25 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+color = (255, 255, 0)
+line_width = 3
+radius = 50
+point = (0, 0)
+# click function will call when there is a click on the screen;
+def click(event,x, y,flags, param):
+	global point, pressed
+	if (event == cv2.EVENT_LBUTTONDOWN):
+		print("Pressed!", x, y)
+		point = (x, y)
+
+cv2.namedWindow("Frame")
+# set the event handler for the onclick event;
+cv2.setMouseCallback("Frame", click)
+
 while(True):
 	ret, frame = cap.read()
-
 	frame = cv2.resize(frame, (0,0), fx=0.5,fy=0.5)
+	cv2.circle(frame, point, radius, color, line_width)
 	cv2.imshow("Frame",frame)
 
 	ch = cv2.waitKey(1)
