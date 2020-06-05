d4037f8ef20b4aed15c675834e319dcf4e637c9e
saobangmath <anhtai2672000@gmail.com>
2020-06-05 12:12:59 +0800

    blur, dilate and erode effects

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_06 Begin/02_06.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_06 Begin/02_06.py
@@ -1,4 +1,21 @@
 import numpy as np
 import cv2
-
+# Gaussian blur filter;
 image = cv2.imread("thresh.jpg")
+cv2.imshow("Original", image)
+# pair (x, y): how much the scale of the blur on each direction (x-axis, y-axis);
+# note that both x, y must be ODD
+blur = cv2.GaussianBlur(image, (5, 1), 0)
+cv2.imshow("Blur", blur)
+# define kernel
+kernel = np.ones((4, 4), "uint8")
+# dilate: turn black (background) pixels -> white pixels
+# erode: turn white pixels -> black (background) pixels;
+dilate = cv2.dilate(image, kernel,iterations= 1)
+erode = cv2.erode(image, kernel, iterations=1)
+
+cv2.imshow("Dilate", dilate)
+cv2.imshow("Erode", erode)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
