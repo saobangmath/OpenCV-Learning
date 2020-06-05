bd7cfff49aba4fd6c218891cf11a14a7fddda9fa
saobangmath <anhtai2672000@gmail.com>
2020-06-05 17:01:28 +0800

    Area, perimeter and curvature

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_07 Begin/03_07.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_07 Begin/03_07.py
@@ -6,7 +6,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 cv2.imshow("Binary", thresh)
 
-_, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 img2 = img.copy()
 index = -1
@@ -16,5 +16,22 @@ color = (255, 0, 255)
 cv2.drawContours(img2, contours, index, color, thickness)
 cv2.imshow("Contours",img2)
 
+objects = np.zeros([img.shape[0],img.shape[1],3], 'uint8')
+for c in contours:
+    cv2.drawContours(objects, [c], -1, color, -1)
+
+    area = cv2.contourArea(c)
+    perimeter = cv2.arcLength(c, True)
+
+    M = cv2.moments(c) # moment of the contour;
+
+    cx = int(M["m10"] / M["m00"]) # x value of the centroid
+    cy = int(M["m01"] / M["m00"]) # y value of the centroid
+
+    cv2.circle(objects, (cx, cy), 4, color, -1)
+
+    print("Area: {}, perimeter: {}".format(area, perimeter))
+
+cv2.imshow("Contours", objects)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
