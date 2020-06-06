70306cde1b52431793b9b3c0a078929a2514455f
saobangmath <anhtai2672000@gmail.com>
2020-06-06 17:43:10 +0800

    eye detection

--- Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch04/04_06 Begin/04_06.py
+++ Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch04/04_06 Begin/04_06.py
@@ -2,3 +2,24 @@ import numpy as np
 import cv2
 
 img = cv2.imread("faces.jpeg",1)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+color = (255, 255, 0)
+
+path ="haarcascade_eye.xml"
+
+eye_cascade = cv2.CascadeClassifier(path)
+eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=2)
+
+print(len(eyes))
+
+for (x, y, w, h) in eyes:
+    xc = (x + x + w) / 2
+    yc = (y + y + h) / 2
+    radius = w / 2
+    cv2.circle(img, (int(xc), int(yc)), int(radius), color = color, thickness = 3)
+
+cv2.imshow("Result", img)
+cv2.imwrite("result.png", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
